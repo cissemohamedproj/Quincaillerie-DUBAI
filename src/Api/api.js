0a9351e96467da889259b8f273bf1f8abfefbbc4
McOriginal <originalmc223@gmail.com>
2026-04-26 06:21:16 +0000

    Changement de URL backend

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// src/api/api.js
 import axios from 'axios';
 
 const api = axios.create({
@@ -25,31 +23,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-=======
-// src/api/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_API_URL ||
-    'https://backend-quincaillerie-dubai.onrender.com/api',
-  // 'http://localhost:5000/api',
-
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-// Ajouter token JWT automatiquement
-api.interceptors.request.use((config) => {
-  const user = localStorage.getItem('authUser');
-  const token = user ? JSON.parse(user).token : null;
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default api;
->>>>>>> f1bfc7f1a5aad4c9643605a512f86cfe3ec0e61f
